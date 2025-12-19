e9cb160f8e19f3ec22e727c97563a52cae5f69a2
Aske <asylbek_2.06@mail.ru>
2025-12-19 15:01:04 +0500

    Add path aliasing to Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,25 +3,31 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import laravel from 'laravel-vite-plugin';
 import { defineConfig } from 'vite';
+import path from 'path';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.tsx'],
-            ssr: 'resources/js/ssr.tsx',
-            refresh: true,
-        }),
-        react({
-            babel: {
-                plugins: ['babel-plugin-react-compiler'],
-            },
-        }),
-        tailwindcss(),
-        wayfinder({
-            formVariants: true,
-        }),
-    ],
-    esbuild: {
-        jsx: 'automatic',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'resources/js'),
     },
+  },
+  plugins: [
+    laravel({
+      input: ['resources/css/app.css', 'resources/js/app.tsx'],
+      ssr: 'resources/js/ssr.tsx',
+      refresh: true,
+    }),
+    react({
+      babel: {
+        plugins: ['babel-plugin-react-compiler'],
+      },
+    }),
+    tailwindcss(),
+    wayfinder({
+      formVariants: true,
+    }),
+  ],
+  esbuild: {
+    jsx: 'automatic',
+  },
 });
